c7c2105f6e10046f866fd611ae0383eb90dd6410
Ihimbru-k <kanihims25@gmail.com>
2024-07-22 14:00:05 +0100

    done with liss

--- dart/data_structures/list.dart
+++ dart/data_structures/list.dart
@@ -19,6 +19,29 @@ void main(){
 
 
 
+  // looping over a list
+
+  var ages = [23, 24, 25, 26, 27,28, 29, 30];
+
+  for(int i =0 ; i< ages.length; i++){
+    print(ages[i]);
+  }
+
+  ages.remove(23);// remove elemen 23 from lis
+  print(ages);
+
+  ages.removeAt(ages.length-1); //remove last element
+  print(ages);
+
+  //looping over list with forEah
+
+  ages.forEach((element) {print(element);});
+
+  ages.shuffle();
+  print(ages);
+
+
+
 
 
 }
