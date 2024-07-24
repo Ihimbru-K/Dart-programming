3ec2072fc86433fea3ec0f69500f1c49a3c232b0
Ihimbru-k <kanihims25@gmail.com>
2024-07-24 22:35:17 +0100

    completed ex 10

--- Ex10_sum_of_num_in_array.dart
+++ Ex10_sum_of_num_in_array.dart
@@ -1,14 +1,31 @@
 
 
 
+
+List<int> arr = [1,2,3,4,5,6,7,8,9,10];
+var sum = 0;
+
+
 void main(){
-  List<int> myNums = [1, 2,3,4,5,6,7,8,9,10];
-  var sum = 0;
-  for(int i = 0; i<= myNums.length-1; i++){
-    sum += myNums[i];
-  }
-  var sum2=0;
-  myNums.forEach((num) { sum2 += num;});
+
+  arr.forEach((element) {sum += element;});
   print(sum);
-  print(sum2);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
