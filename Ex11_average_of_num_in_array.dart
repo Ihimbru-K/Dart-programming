b8e9daef5e8d4c65a54825c120c22dbc6dda5ab7
Ihimbru-k <kanihims25@gmail.com>
2024-07-24 22:58:14 +0100

    completed exercises 11 and 12

--- Ex11_average_of_num_in_array.dart
+++ Ex11_average_of_num_in_array.dart
@@ -1,7 +1,30 @@
-void main() {
-  List<num> nums = [1, 45, 22.0, 35, 21, 20];
-  num sum = 0;
+// void main() {
+//   List<num> nums = [1, 45, 22.0, 35, 21, 20];
+//   num sum = 0;
+//   double avg = 0;
+//   nums.forEach((number) { sum += number; avg = sum/nums.length;});
+//   print(avg);
+// }
+
+
+
+double AverageNumInArr(List<int> arr){
+
+  int sum = 0;
   double avg = 0;
-  nums.forEach((number) { sum += number; avg = sum/nums.length;});
-  print(avg);
+  arr.forEach((element) {
+    sum += element;
+  }
+
+  );
+  avg = sum / arr.length;
+  return avg;
+  
+}
+
+void main(){
+
+
+  print(AverageNumInArr([1,2,3,4,5,6,7,8,9,10]));
+  
 }
--- Ex12_max_num_in_array_of_num.dart
+++ Ex12_max_num_in_array_of_num.dart
@@ -1,16 +1,16 @@
 
-void main(){
-  List<int> nums = [1,2,3,4,5,6,7,8,9,10];
-   num max = 0;
-  for(int i = 0; i< nums.length-1; i++){
-    if(nums[i] > nums[i+1]){
-      max = nums[i];
-    }
-    else{
-      max = nums[i+1];
-    }
-  }
-  print(max);
+// void main(){
+//   List<int> nums = [1,2,3,4,5,6,7,8,9,10];
+//    num max = 0;
+//   for(int i = 0; i< nums.length-1; i++){
+//     if(nums[i] > nums[i+1]){
+//       max = nums[i];
+//     }
+//     else{
+//       max = nums[i+1];
+//     }
+//   }
+//   print(max);
 
 
 
@@ -18,5 +18,36 @@ void main(){
   // nums.forEach((num) {
   //   if(nums[num] > nums[num+1]){max = nums[num];}
   //   else{max = nums[num+1];}; });
+
+//}
+
+
+
+import 'dart:math';
+
+int MaxNumInArray(List<int> arr){
+
+  int max = arr[0];
+
+  for(int i = 0; i< arr.length-1; i++){
+
+    if(max < arr[i]){
+      max = arr[i];
+
+    }
+
+
+
+  }
+return max;
+
+
+}
+
+
+void main(){
+
+  print(MaxNumInArray([1,45, 23, 22]));
+
 
 }
