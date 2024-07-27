628f911d74e46dee31a6181248861e593f302f36
Ihimbru-k <kanihims25@gmail.com>
2024-07-27 19:27:19 +0100

    ex 14

--- Ex13_fibonacci_without_recursion.dart
+++ Ex13_fibonacci_without_recursion.dart
@@ -1,20 +1,45 @@
-
+//
+//
+//
+// void main(){
+//   var a = 0;
+//   var b = 1;
+//   var n = 50;
+//
+//   print(a);
+//   print(b);
+//
+//   for(int i = 2; i<=n; i++){
+//     var c = a + b;
+//     print(c);
+//     a = b;
+//     b = c;
+//   }
+//
+//
+// }
 
 
 void main(){
-  var a = 0;
-  var b = 1;
-  var n = 50;
 
-  print(a);
-  print(b);
+  var a = 1;
+  var b = 2;
+  var c = 0;
 
-  for(int i = 2; i<=n; i++){
-    var c = a + b;
+  for(int i = 0; i<=9; i++){
+    print(a);
+    print(b);
+    c = a +b;
     print(c);
     a = b;
-    b = c;
+    b =c;
+
+
+
+
   }
+
+
 
 
 }
