89aae06b2e7cd16978ac0541d30d589351e55a70
Ihimbru-k <kanihims25@gmail.com>
2024-07-27 19:48:23 +0100

    completed exercise 14

--- ex14_nth_fibonacci_using_recursion.dart
+++ ex14_nth_fibonacci_using_recursion.dart
@@ -1,3 +1,30 @@
 
 
+int nth_fibonacci(int n){
+
+  if(n == 0){
+    return 0;
+  }else if (n == 1){
+
+    return 1;
+  }
+  else return nth_fibonacci(n-1) + nth_fibonacci( n - 2);
+
+
+}
+
+void main(){
+
+  int n = 50;
+  for(int i = 0; i<= n; i++ ){
+
+    print(nth_fibonacci(n));
+  }
+
+  print(nth_fibonacci(55));
+
+
+
+
+}
 
