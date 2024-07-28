695f822eba844b7634b7c69357496dacbd2142b0
Ihimbru-k <kanihims25@gmail.com>
2024-07-28 11:32:31 +0100

    working on reversing array

---
+++ ex22_reverse_array.dart
@@ -0,0 +1,26 @@
+
+
+
+List<int> ReverseArray(List<int> arr){
+
+  List<int> rev_arr = [];
+
+  for(int i = arr.length-1 ; i>= 0 ; i--){
+    for(int j = 0; j<= rev_arr.length; j++){
+
+      rev_arr[j] = arr[i];
+    }
+
+  }
+
+  return rev_arr;
+}
+
+
+void main (){
+
+  print(ReverseArray([1,2,3,4,5,6,7,8,9,10]));
+
+
+}
+
