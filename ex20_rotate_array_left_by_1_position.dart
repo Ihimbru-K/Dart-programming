054e0c8cee9d5c0a202ba49fd4321d2c2df96020
Ihimbru-k <kanihims25@gmail.com>
2024-07-28 10:40:11 +0100

    completed exercise 20

--- ex20_rotate_array_left_by_1_position.dart
+++ ex20_rotate_array_left_by_1_position.dart
@@ -5,12 +5,23 @@ List<int> RotateLeft(List<int> arr){
 
   for(int i = 0; i<= arr.length; i++ ){
 
-    arr.removeAt(i);
-    
+  int first_element =   arr.removeAt(0);
+    arr.add(first_element);
+   // arr[i] = arr[i+1];
+
+
 
   }
+  return arr;
 
 
 
+
+}
+
+
+void main(){
+
+  print(RotateLeft([1,2,3,4,5,6]));
 
 }
