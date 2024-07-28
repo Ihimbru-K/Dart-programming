76b75ecdd1c642214b930f5d9bdf6c57e644b496
Ihimbru-k <kanihims25@gmail.com>
2024-07-28 13:02:07 +0100

    refactoring code

--- ex24_array_of_distinct_numbers_in_2_arrays.dart
+++ ex24_array_of_distinct_numbers_in_2_arrays.dart
@@ -1,6 +1,57 @@
 
 
 
+List<int> ReturnArrayOfDistinctNums(List<int> arr1, List<int> arr2){
+
+
+  List<int> newArr = [];
+  bool foundDuplicate = false;
+  
+
+
+
+
+  // for(int i = 0; i< arr1.length; i++){
+  //   for(int j = 0; j< arr2.length; j++){
+  //     if(arr1[i] == arr2[j]){
+  //       foundDuplicate = true;
+  //
+  //       break;
+  //     }
+  //
+  //     if (!foundDuplicate){
+  //       newArr.add(arr1[i]);
+  //     }
+  //
+  //
+  //     for(int j = 0 ; j< arr2.length; j++){
+  //       if(!newArr.contains(arr2[j])){
+  //         newArr.add(arr2[j]);
+  //
+  //       }
+  //     }
+  //
+  //
+  //   }
+  // }
+  return newArr;
+
+
+}
+
+
+void main (){
+
+  print(ReturnArrayOfDistinctNums([1,3, 4], [1,4,5,6]));
+
+
+
+
+
+
+
+}
+
 
 
 
