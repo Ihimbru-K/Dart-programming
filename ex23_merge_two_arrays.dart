7c50a1e7f70fb2bc5cc51f5a282f2aadd17ad4ed
Ihimbru-k <kanihims25@gmail.com>
2024-07-28 12:23:24 +0100

    completed exercise 23

--- ex23_merge_two_arrays.dart
+++ ex23_merge_two_arrays.dart
@@ -1,4 +1,28 @@
 
 
 
+List<int> MergeArrays(List<int> arr1, List<int> arr2){
+
+  List<int> newArr = [];
+
+  for(int i= 0; i< arr1.length; i++){
+    newArr.add(arr1[i]);
+  }
+  for(int j = 0; j< arr2.length; j++){
+    newArr.add(arr2[j]);
+  }
+  newArr.sort();
+
+  return newArr;
+}
+
+void main(){
+
+  print(MergeArrays([1,2,3,4,5], [0,2,45]));
+
+
+}
+
+
+
 
