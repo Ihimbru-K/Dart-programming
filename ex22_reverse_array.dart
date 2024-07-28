e22311d5d275004449c6ead7697d0c7c8964a953
Ihimbru-k <kanihims25@gmail.com>
2024-07-28 12:01:21 +0100

    reversed array through temp array and swapping

--- ex22_reverse_array.dart
+++ ex22_reverse_array.dart
@@ -17,7 +17,33 @@ List<int> ReverseArray(List<int> arr){
 void main (){
 
   print(ReverseArray([1,2,3,4,5,6,7,8,9,10]));
+  print(revArr([10,20,30,40]));
 
 
 }
 
+
+List<int> revArr(List<int> arr2){
+
+  var start = 0;
+  var end = arr2.length-1;
+
+  while(start < end){
+
+    var temp = arr2[start];
+    arr2[start] = arr2[end];
+    arr2[end] = temp;
+
+    start ++;
+    end --;
+
+
+  }
+  return arr2;
+
+
+
+
+
+
+}
