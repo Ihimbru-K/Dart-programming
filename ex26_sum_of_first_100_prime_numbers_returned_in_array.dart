0670eb019e9e4ca18d3c897ee8eb48c3c44e46c3
Ihimbru-k <kanihims25@gmail.com>
2024-07-29 23:56:06 +0100

    completed exercise 28

--- ex26_sum_of_first_100_prime_numbers_returned_in_array.dart
+++ ex26_sum_of_first_100_prime_numbers_returned_in_array.dart
@@ -1,15 +1,41 @@
 
 
+import 'dart:math';
+
+List<int> ReturnFirstNPrimeNumInArray(int n){
+
+  List<int> primeNumArr = [];
+  int num = 1;
 
 
 
-
+  while(num  < n ) {
+    bool isPrime = true;
+    for (int j = 2; j <= sqrt(num); j++) {
+      if (num % j == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if(isPrime){
+      primeNumArr.add(num);
+    }
+    num++;
+  }
+  return primeNumArr;
+}
 
 
 
 void main(){
 
+  print(ReturnFirstNPrimeNumInArray(100));
+
 
 
 
 }
+
+
+
+
