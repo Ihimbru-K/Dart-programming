f07aa7afd5bfd30484238d83e5546c75dd9d0c9d
Ihimbru-k <kanihims25@gmail.com>
2024-07-30 00:57:18 +0100

    fixed it

--- ex27_distance_between_first_100_prime_numbers.dart
+++ ex27_distance_between_first_100_prime_numbers.dart
@@ -1,7 +1,32 @@
+import 'dart:math';
 
+List<int> returnFirstNPrimeNumInArray(int n) {
+  List<int> primeNumArr = [];
+  int num = 2; // Starting from 2, as 1 is not a prime number
 
+  while (primeNumArr.length < n) {
+    bool isPrime = true;
+    for (int j = 2; j <= sqrt(num); j++) {
+      if (num % j == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primeNumArr.add(num);
+    }
+    num++;
+  }
 
-void main(){
+  for (int k = 0; k < primeNumArr.length - 1; k++) {
+    var gap = primeNumArr[k + 1] - primeNumArr[k];
+    print("Gap between ${primeNumArr[k]} and ${primeNumArr[k + 1]} is $gap");
+  }
 
-  
+  return primeNumArr;
+}
+
+void main() {
+  List<int> primes = returnFirstNPrimeNumInArray(100);
+  print("First 100 prime numbers: $primes");
 }
