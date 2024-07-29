e47a0c1be7d38761d7448b4be468110f34174e4f
Ihimbru-k <kanihims25@gmail.com>
2024-07-29 23:09:58 +0100

    working on e 27

--- ex25_accept_array_and_return_distinct_array.dart
+++ ex25_accept_array_and_return_distinct_array.dart
@@ -1,8 +1,47 @@
 
 
+
+
+List<int> ReturnDistinctArr(List<int> arr){
+
+  for(int i = 0; i< arr.length; i++){
+    if(arr[i] == arr[i+1] ){
+
+      arr.removeAt(arr[i+1]);
+
+    }
+
+  }
+  return arr;
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
 void main(){
 
 
 
-  
+print(ReturnDistinctArr([1,2,2,2,4]));
+
+
+
+
+
+
+
+
+
 }
