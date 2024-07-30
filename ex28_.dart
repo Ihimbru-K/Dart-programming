ee2a20fc03b0c5606fc0869d0b498ab215282655
Ihimbru-k <kanihims25@gmail.com>
2024-07-30 23:43:21 +0100

    completed exercise 30

--- ex28_.dart
+++ ex28_.dart
@@ -1,9 +1,33 @@
 
 
 
+String AddStringNum(String num1, String num2){
+
+  var num1Int = int.parse(num1);
+  var num2Int = int.parse(num2);
+
+  var sum = num1Int + num2Int;
+  var sumString = sum.toString();
+
+  return sumString;
+
+
+
+
+
+
+}
+
+
+
+
+
 
 void main(){
+  print(AddStringNum("123466666", "98765432"));
 
 
-  
+
+
+
 }
