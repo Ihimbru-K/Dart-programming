4149833f57e33bdbc60801116d9434ac6bd0f7d3
Ihimbru-k <kanihims25@gmail.com>
2024-07-31 00:36:33 +0100

    completed exercise 32

--- ex30_capitalize_first_letter_in_words_in_String.dart
+++ ex30_capitalize_first_letter_in_words_in_String.dart
@@ -1,2 +1,45 @@
 
 
+
+
+
+String FirstLettersOfWordsToUpperCase(String text){
+
+
+
+
+  List<String> words = text.split(RegExp(r'\s+|,\s*'));
+
+  for(int i = 0; i< words.length; i++){
+   if(words[i].isNotEmpty){
+     words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+   }
+
+
+  }
+  return words.join(' ');
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+void main(){
+
+ var text = FirstLettersOfWordsToUpperCase("i am a boy");
+ print(text);
+
+
+
+
+}
