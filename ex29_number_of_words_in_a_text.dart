3033f18923e4b86c5062e88121891567165f93cf
Ihimbru-k <kanihims25@gmail.com>
2024-07-30 23:56:41 +0100

    initial implementation of ex 31

--- ex29_number_of_words_in_a_text.dart
+++ ex29_number_of_words_in_a_text.dart
@@ -1,10 +1,39 @@
 
 
 
+int ReturnNumberOfWordsInAText(String text){
+
+  int sum = 0;
+
+  for(int i = 0; i< text.length; i++){
+
+    if(text.contains(" ") || text.contains(",")){
+      sum ++;
+
+
+    }
+
+
+  }
+  return sum;
+
+
+
+
+
+}
+
+
+
+
+
+
+
 
 
 
 void main(){
+  print(ReturnNumberOfWordsInAText("I am a boy "));
 
 
 
