1ba72268bbdc056dc7739485876fde155e12c63d
Ihimbru-k <kanihims25@gmail.com>
2024-08-15 20:42:39 +0100

    completed this

--- ex31_sum_of_numbers_received_in_a_comma_delimited_string.dart
+++ ex31_sum_of_numbers_received_in_a_comma_delimited_string.dart
@@ -1,15 +1,29 @@
 
 
 
+int SumNumbersInCommaDelimitedString(String string){
+
+  List<String> splitString = string.split(RegExp(r','));
+
+
+  var sum = 0;
+  for(int i = 0; i<splitString.length; i++){
+
+    sum += int.parse(splitString[i]);
+
+  }
+
+  return sum;
 
 
 
 
+}
 
 
 void main(){
 
-
+print(SumNumbersInCommaDelimitedString("5,1,4"));
 
 
 
