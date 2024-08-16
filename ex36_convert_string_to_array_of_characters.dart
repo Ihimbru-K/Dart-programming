be1e15a69b21a8d05cbec2ee3f23cd45196ab7d2
Ihimbru-k <kanihims25@gmail.com>
2024-08-16 12:15:14 +0100

    completed exercise 36

--- ex36_convert_string_to_array_of_characters.dart
+++ ex36_convert_string_to_array_of_characters.dart
@@ -2,4 +2,28 @@
 
 
 
-void main(){}
+List<String> StringToArrayOfCharacters(String string){
+
+
+  List<String> array_of_characters = string.split(RegExp(r''));
+  return array_of_characters;
+
+
+
+
+
+
+
+
+}
+
+
+
+void main(){
+
+  print(StringToArrayOfCharacters("Characteristics"));
+
+
+
+
+}
