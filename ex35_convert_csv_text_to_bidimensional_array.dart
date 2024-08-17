6a81c1c1c945c355da1e18943173e11fae3909b7
Ihimbru-k <kanihims25@gmail.com>
2024-08-17 23:50:39 +0100

    worked on 36

--- ex35_convert_csv_text_to_bidimensional_array.dart
+++ ex35_convert_csv_text_to_bidimensional_array.dart
@@ -1,9 +1,23 @@
 
-List<List<String>> CsvToBidimensionalArray(){
+import 'dart:collection';
+import 'dart:isolate';
+
+Iterable<List<String>> CsvToBidimensionalArray(String string){
+
+  Iterable<List<String>> biarr = string.split('\n').map((line) => line.split(',').toList() );
+  return biarr;
 
 
 }
 
 
 
-void main(){}
+void main(){
+  
+  print(CsvToBidimensionalArray('''Name, Age, Country
+Alice, 25, USA
+Bob, 30, Canada
+Charlie, 22, UK'''));
+
+  
+}
