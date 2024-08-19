a84749b2f3f59454bc45b8ccc1194a8a7530918d
Ihimbru-k <kanihims25@gmail.com>
2024-08-19 21:13:31 +0100

    bubble sor algo done

--- ex40_bubble_sort_algorithm.dart
+++ ex40_bubble_sort_algorithm.dart
@@ -5,20 +5,27 @@
 
 List<dynamic> BubbleSort(List<dynamic> inputArray){
 
+ var n = inputArray.length;
 
-  for(int i = 0; i< inputArray.length; i++){
+  bool swapped = false;
+  do{
 
-    if(inputArray[i] > inputArray[i+1]){
+    for(int i = 0; i< n-1; i++){
 
-      inputArray[i+1] = inputArray[i];
-      i++;
+      if(inputArray[i] > inputArray[i+1]){
+
+        var s = inputArray[i];
+        inputArray[i] = inputArray[i+1];
+        inputArray[i +1 ] = s;
+        swapped = true;
+
+      }
 
     }
-    }
+    n--;
+  }while(swapped);
   return inputArray;
-
-
-
+  
 }
 
 
