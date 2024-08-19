8e8b4c8a90d32f4d6025ca4b01f640d0603626d7
Ihimbru-k <kanihims25@gmail.com>
2024-08-19 13:39:56 +0100

    completed exercise 37

--- ex37_convert_Strings_to_an_array_of_ascii_codes_of_each_.dart
+++ ex37_convert_Strings_to_an_array_of_ascii_codes_of_each_.dart
@@ -1,8 +1,44 @@
 
 
 
+
+
+
+List<int> StringToAsciiArray(String string){
+
+  List<int> asciiArray = [];
+
+  for(int i = 0; i< string.length; i++){
+    
+   // asciiArray.add(String.fromCharCode(charCode));
+    //asciiArray.add(i.cod)
+
+   // print(string.codeUnits);
+    asciiArray.add(string.codeUnitAt(i));
+
+  }
+
+ 
+
+  return asciiArray;
+
+
+
+
+
+
+
+
+}
+
+
+
+
 void main(){
 
 
-  
+  print(StringToAsciiArray("Hello"));
+
+
+
 }
